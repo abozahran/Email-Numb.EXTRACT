99228fd1f9450a6d35ce30d201cdebf648cd43cb
abozahran <107565623+abozahran@users.noreply.github.com>
2025-07-03 15:06:08 +0300

    Delete components directory

--- components/ToastMessage.jsx
+++
@@ -1,7 +0,0 @@
-export default function ToastMessage({ message, color = 'green' }) {
-  return (
-    <div className={`fixed bottom-5 right-5 p-4 rounded-lg text-white shadow-lg ${color === 'red' ? 'bg-red-500' : 'bg-green-600'}`}>
-      {message}
-    </div>
-  );
-}
